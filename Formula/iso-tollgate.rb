06aa12ad5c6620a9dbec4d73c58b1da4c8235fa2
ArunMishra1 <15990299+ArunMishra1@users.noreply.github.com>
2026-06-22 22:24:09 -0400

    Bump to 0.1.1, fix audit findings, harden brew test

--- Formula/iso-tollgate.rb
+++ Formula/iso-tollgate.rb
@@ -3,8 +3,8 @@ class IsoTollgate < Formula
 
   desc "Pre-submission safety gate for ISO 20022 pacs.008 payment messages"
   homepage "https://github.com/iso-tollgate/tollgate"
-  url "https://files.pythonhosted.org/packages/a3/78/2831498f7459be58619d3b1f973d16d49f18c587d794ae611d87e8a02815/iso_tollgate-0.1.0.tar.gz"
-  sha256 "c77d68fcf27c8e92e939d88a944a31fb5de5e4ff1cc475b1981aebd77c3f28d9"
+  url "https://files.pythonhosted.org/packages/f5/68/b9f488d2e310eae49b375cfdee295b6bd7c43fd66c3fcd1464613fa673ad/iso_tollgate-0.1.1.tar.gz"
+  sha256 "7734ffab03e749eb2b3c2a2f37081ff1ca405aa3ac1c41b6735e36d4b3e17fb1"
   license "Apache-2.0"
 
   depends_on "rust" => :build # jiter and pydantic_core are Rust extensions built
